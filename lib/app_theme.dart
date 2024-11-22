d7d9d87c87abfba997e0640a11e9da1a99eda074
Ahmed Nazeer <ahmed.m.nazeer.18@gmail.com>
2024-11-22 19:41:15 +0200

    Finish home screen

---
+++ lib/app_theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color lightPrimary = Color(0xFFB7935F);
+  static const Color darkPrimary = Color(0xFF141A2E);
+  static const Color white = Color(0xFFF8F8F8);
+  static const Color black = Color(0xFF242424);
+  static const Color gold = Color(0xFFFACC1D);
+
+  static ThemeData lightTheme = ThemeData(
+    primaryColor: lightPrimary,
+    scaffoldBackgroundColor: Colors.transparent,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.transparent,
+      centerTitle: true,
+      toolbarHeight: 75,
+      elevation: 0,
+      titleTextStyle: TextStyle(
+        color: black,
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+        // fontFamily: 'El Messiri',
+      ),
+    ),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      type: BottomNavigationBarType.fixed,
+      selectedItemColor: black,
+      unselectedItemColor: white,
+      backgroundColor: lightPrimary,
+      
+    ),
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    primaryColor: darkPrimary,
+  );
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:islami/tabs/hadeth/hadeth_tab.dart';
+import 'package:islami/tabs/quran/quran_tab.dart';
+import 'package:islami/tabs/radio/radio_tab.dart';
+import 'package:islami/tabs/sebha/sebha_tab.dart';
+import 'package:islami/tabs/settings/settings_tab.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+  static const String routeName = '/home';
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedCurrentIndex = 0;
+  final List<Widget> _screens = [
+    const QuranTab(),
+    const HadethTab(),
+    const SebhaTab(),
+    const RadioTab(),
+    const SettingsTab(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/bg.png'),
+          fit: BoxFit.fill,
+        ),
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('إسلامي'),
+        ),
+        body: _screens[selectedCurrentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: selectedCurrentIndex,
+          onTap: (index) => {
+            selectedCurrentIndex = index,
+            setState(() {}),
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage('assets/images/moshaf.png'),
+                size: 30,
+              ),
+              label: 'القران',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage('assets/images/hadeth.png'),
+              ),
+              label: 'الحدبث',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage('assets/images/sebha.png'),
+                size: 30,
+              ),
+              label: 'سبحه',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage('assets/images/radio.png'),
+              ),
+              label: 'الراديو',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings_outlined),
+              label: 'الاعدادات',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
